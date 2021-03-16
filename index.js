635af61088d661cabf7e730115f5203298b28b5f
santiago-rodrig <santo1996.29@gmail.com>
2021-03-16 18:27:25 -0400

    parse and log request http method

--- index.js
+++ index.js
@@ -13,10 +13,12 @@ var server = http.createServer(function(req, res) {
   // Get the path
   var path = parsedUrl.pathname
   var trimmedPath = path.replace(/^\/+|\/+$/g, '')
+  // Get the HTTP Method
+  var method = req.method.toLowerCase()
   // Send the response
   res.end('Hello World\n')
   // Log the request path
-  console.log('Request received on path: ' + trimmedPath)
+  console.log('Request received on path: ' + trimmedPath + ' with this method: ' + method)
 })
 
 // Start the server, and have it listen on port 3000
